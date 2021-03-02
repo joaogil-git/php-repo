51b5c0a8fc4c2f7d9bf4de360e815d899114ec95
João Gilberto <joaogilberto.silva@viannasempre.com.br>
2021-03-01 23:32:42 -0300

    Exercício for

---
+++ MariAna.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marai</title>
+</head>
+<body>
+
+<?php
+for($i=1;$i<=99;$i++){
+?>
+
+Mariana conta <?=$i?> <br>
+Mariana conta <?=$i?>
+
+<?php for($j=1; $j<=$i; $j++){
+?>
+    é <?=$j?>,<?php
+};
+?> <br>
+
+
+Ana viva a Mariana viva a Mariana. <br><br>
+
+<?php
+};
+?>
+
+</body>
+</html>
